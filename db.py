848b32512dd8302a1140b25c7d9eb4cb664b28a4
Arno Volts <thebluehermit@protonmail.com>
2021-04-13 16:34:46 +0200

    working program

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
 import base64
 import os
+import sqlite3
+import hashlib
+from getpass import getpass
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import sqlite3
 
 def init():
    # Create database
@@ -12,8 +14,8 @@ def init():
    #cursor
    cursor = con.cursor()
 
-   # Create table 
-   table = """
+   # Create tables 
+   crendentials_table = """
       CREATE TABLE IF NOT EXISTS Credentials(
          username TEXT NOT NULL,
          platform TEXT NOT NULL,
@@ -22,7 +24,35 @@ def init():
       );
    """
 
-   cursor.execute(table)
+   user_table = """ 
+      CREATE TABLE IF NOT EXISTS User(
+         username TEXT NOT NULL,
+         password BLOB NOT NULL,
+         salt BLOB NOT NULL,
+         UNIQUE(username)
+      );
+   """
+
+   cursor.execute(crendentials_table)
+   cursor.execute(user_table)
+
+   # ask for master password 
+   print("Create app user")
+   while True:
+      username = input("username")
+      password = getpass("Master password: ")
+      confirm_password = getpass("Confirm password: ")
+
+      if password == confirm_password: 
+         salt = os.urandom(32)
+         key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+         break
+
+
+   cursor.execute(""" 
+      INSERT INTO User VALUES(?, ?, ?);
+   """, (username, key,salt))
+   
    con.commit()
 
    # Close connection to the database
@@ -37,7 +67,6 @@ def encrypt(masterPwd, password):
       iterations=100000,
    )
 
-   masterPwd = bytes(masterPwd, encoding="utf-8")
    key = base64.urlsafe_b64encode(kdf.derive(masterPwd))
    f = Fernet(key)
    encryptedPass = f.encrypt(bytes(password,encoding="utf-8"))
@@ -52,7 +81,6 @@ def decrypt(masterPwd, salt, password):
       iterations=100000,
    )
 
-   masterPwd = bytes(masterPwd, encoding="utf-8")
    key = base64.urlsafe_b64encode(kdf.derive(masterPwd))
    f = Fernet(key)
 
@@ -64,7 +92,7 @@ def write(data):
    cursor = con.cursor()
       
    # Insert data into database
-   cursor.execute("INSERT INTO Credentials VALUES (?, ?, ?, ?)", (data[0], data[1], data[2], data[3]))  
+   cursor.execute("INSERT INTO Credentials VALUES (?, ?, ?, ?)", (data["username"], data["platform"], data["password"], data["salt"]))  
       
    # Save the changes
    con.commit()
@@ -91,9 +119,86 @@ def search_username(username):
    con.close()
    return rows
 
+def update_username(username, platform, new_username):
+   con = sqlite3.connect("vault.db")
+   con.row_factory = sqlite3.Row
+   cursor = con.cursor()
+
+   rows = cursor.execute(""" 
+      UPDATE Credentials 
+      SET username = ? 
+      WHERE username = ? AND platform = ?""", (new_username, username, platform,))
+      
+   con.commit()
+   con.close()
+
+def update_password(username, platform, new_password, new_salt):
+   con = sqlite3.connect("vault.db")
+   con.row_factory = sqlite3.Row
+   cursor = con.cursor()
+
+   rows = cursor.execute("""
+      UPDATE Credentials 
+      SET password = ?, 
+         salt = ?
+      WHERE username = ? AND platform = ?""", (new_password, new_salt,username, platform,))
+
+   con.commit()
+   con.close()
+
+def update_masterpass(new_pass, new_salt, username, old_master):
+   con = sqlite3.connect("vault.db")
+   con.row_factory = sqlite3.Row
+   cursor = con.cursor()
+
+   rows = cursor.execute("UPDATE user set password = ? , salt = ? WHERE username = ?", (new_pass, new_salt,username)).fetchone()
+
+   old_data = cursor.execute("SELECT * FROM Credentials")
+
+   for row in old_data:
+      salt = row["salt"]
+      old_encrypt_pass = str(decrypt(old_master, salt, row["password"]))
+      new_encrypt = encrypt(new_pass, old_encrypt_pass)
+      cursor.execute(""" 
+         UPDATE Credentials
+         SET password = ?,
+             salt = ?
+         WHERE username = ? AND platform = ?
+      
+      """,( new_encrypt["encryptedPass"], 
+            new_encrypt["salt"],
+            row["username"],
+            row["platform"]
+         ))
+
+   con.commit()
+   con.close()
 
 def checkDB():
    """
    code found at  
    https://careerkarma.com/blog/python-check-if-file-exists/ """
    return os.path.isfile("vault.db")
+
+
+def check_user(password, username):
+   """ 
+      Source : 
+
+      https://nitratine.net/blog/post/how-to-hash-passwords-in-python/
+    """
+   con = sqlite3.connect("vault.db")
+   con.row_factory = sqlite3.Row
+   cursor = con.cursor()
+
+   rows = cursor.execute("SELECT * FROM User WHERE username = ?", (username,)).fetchone()
+
+   salt = rows["salt"]
+
+   key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+   if key == rows["password"] and username == rows["username"]:
+      return key
+
+   return None
+
--- vault.py
+++ vault.py
@@ -1,6 +1,10 @@
 import click
 import db
 import pyperclip
+import hashlib
+import getpass
+import sys
+import os
 
 
 @click.group()
@@ -11,7 +15,8 @@ def cli():
 
 @click.command()
 def init_app():
-    """ Creates the databases needed for the app. """
+    """ Creates the databases needed for the app and creates a user. """
+    
     db.init()
     click.echo("database created, you are ready to go.")
 
@@ -19,19 +24,30 @@ def init_app():
 @click.command()
 def add_entry():
     """ Allows for adding credentials into the database """
-     # Verify that the database exists
+    # Verify that the database exists
     if db.checkDB():
         click.echo("please fill in all the fields.")
+        
+        # Veriyf user
+        username = click.prompt("Master username ")
+        masterPwd = getpass.getpass("Master password: ")
+        masterPwd = db.check_user(masterPwd, username)
+        
+        if masterPwd == None:
+            click.echo("Wrong password !!")
+            sys.exit()
+
+        # Get data from user and store them in db
         while True:
             data = get_entry()
 
             click.echo("please confirm entry")
             click.echo(data["username"] + ", " +
-                       data["platform"] + ", " + data["password"])
+                       data["platform"])
             confimation = confirm()
 
             if confimation == 'y':
-                masterPwd = click.prompt("Enter master Password")
+
                 encrypt = db.encrypt(masterPwd, data["password"])
                 data["password"] = encrypt["encryptedPass"]
                 data["salt"] = encrypt["salt"]
@@ -44,26 +60,36 @@ def add_entry():
 
 @click.command()
 @click.argument("platform")
-def get_platform(platform):
+def search_by_platform(platform):
     """Gets the platform you wish to get the crediantials for."""
     if db.checkDB():
+        # Verify user
+        Click.echo("Login")
+        username = click.prompt("Master username")
+        masterPwd =  masterPwd = getpass.getpass("Master password: ")
+        masterPwd = db.check_user(masterPwd, username)
+        
+        if masterPwd == None:
+            click.echo("Wrong password !!")
+            sys.exit()
 
         # search for data
         data = db.search_platform(platform)
         if data is not None:
-            # click.echo("username " + data['username'])
-            masterPwd = click.prompt("Enter master password")
+            click.echo("username " + data['username'])
+
             try:
                 password = db.decrypt(
                     masterPwd, data["salt"], data["password"])
+               
+                # copy password to clipboard
+                pyperclip.copy(password.decode())
+                
+                #  warn user that the password has been copied to the clipboard
+                click.echo("Password has been copied into clipboard !")
             except:
-                click.echo("The master password is incorrect.")
+                click.echo("Something went wrong :(")
 
-            # copy password to clipboard
-            pyperclip.copy(password.decode())
-
-            #  warn user that the password has been copied to the clipboard
-            click.echo("Password has been copied into clipboard !")
         else:
             click.echo("platform didn't match check your spelling.")
     else:
@@ -72,20 +98,30 @@ def get_platform(platform):
 
 @click.command()
 @click.argument("username")
-def get_username(username):
+def search_by_username(username):
     """Gets the username for the account you wish to get the crediantials for."""
     if db.checkDB():
+        # Verify user
+        master_username = click.prompt("master username")
+        masterPwd = masterPwd = getpass.getpass("Master password: ")
+        masterPwd = db.check_user(masterPwd, master_username)
+        
+        if masterPwd == None:
+            click.echo("Wrong password !!")
+            sys.exit()
+
         # search for data
         data = db.search_username(username)
-        if data is not None:
-            click.echo("username " + data['username'])
-            masterPwd = click.prompt("Enter master password")
 
+        if data is not None:
+            click.echo("Credentials")
+            click.echo("Platform: " + data["platform"])
+            
             try:
                 password = db.decrypt(
                     masterPwd, data["salt"], data["password"])
             except:
-                click.echo("The master password is incorrect.")
+                click.echo("Something went wrong :(")
 
             # copy password to clipboard
             pyperclip.copy(password.decode())
@@ -97,10 +133,79 @@ def get_username(username):
     else:
         click.echo("Please run 'vault init' to create the database ")
 
+@click.command()
+@click.option('-p', default=False, help="If true updates password for an account")
+@click.option('-u', default=False, help="If true updates username for an account")
+@click.option('-mp', default=False, help="If true updates master password")
+def update(p, u, mp):
+    """ Allows to update data """
+    #Verify user
+    master_username = click.prompt("Master username")
+    masterPwd = masterPwd = getpass.getpass("Master password: ")
+    masterPwd = db.check_user(masterPwd, master_username)
+
+    if masterPwd == None:
+        click.echo("Wrong master Password")
+        sys.exit()
+
+    if p:
+        username = click.prompt("Username")
+        platform = click.prompt("platform")
+        new_pass = getpass.getpass("New password: ")
+        confirm_pass = getpass.getpass("confirm password: ")
+
+        if new_pass == confirm_pass:
+            encrypted_data = db.encrypt(masterPwd, new_pass)
+
+            try:
+                db.update_password(username, platform, encrypted_data["encryptedPass"], encrypted_data["salt"])
+                click.echo("password updated !")
+            except:
+                click.echo("Something went wrong try again...")
+        else: 
+            click.echo("Passwords didn't match.")
+
+    elif u: 
+        username = click.prompt("Old username ")
+        platform = click.prompt("platform ")
+        new_username = click.prompt("New username")
+        try:
+            db.update_username(username, platform, new_username)
+            click.echo("username updated !")
+        except:
+            click.echo("Something went wrong trya again...")
+    
+    elif mp:
+        password = getpass.getpass("new password: ")
+        confirm_password = getpass.getpass("Confirm new password: ")
+        
+        if password == confirm_password: 
+            salt = os.urandom(32)
+            key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+            try: 
+                db.update_masterpass(key, salt, master_username,masterPwd)
+
+            except:
+                click.echo("Something went wrong try again...")
+
+        else:
+            click.echo("Passwords didn't match.")
+            sys.exit()
+
+
+
 def get_entry():
-    username = input("username: ")
-    platform = input("platform: ")
-    password = input("password: ")
+    while True:
+        username = input("username: ")
+        platform = input("platform: ")
+        password = getpass.getpass()
+        confirm_password = getpass.getpass('confirm password:')
+
+        if password == confirm_password:
+            break
+        else: 
+            click.echo("Passwords did not match :(")
 
     return {
         "username": username,
@@ -121,5 +226,6 @@ def confirm():
 
 cli.add_command(init_app)
 cli.add_command(add_entry)
-cli.add_command(get_platform)
-cli.add_command(get_username)
+cli.add_command(search_by_platform)
+cli.add_command(search_by_username)
+cli.add_command(update)
